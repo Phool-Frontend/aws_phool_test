cd0f91c87f87d3a6a7ad4b1161b5a5d7ad0ce901
Phool Herrera Condezo <59568224+Phool-Frontend@users.noreply.github.com>
2023-09-30 22:27:50 -0500

    [Add] - api get for aws

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ app.listen(port, () => {
     console.log(`Conectado Servidor      ---> localhost:${port}`);
     console.log("Me gusta la moto patagonia :3");
 });
+
+app.get('/',(req,res) => res.json("The nex year 2023 i'm job in united states"));
